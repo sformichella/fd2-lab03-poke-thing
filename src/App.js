8158a001949b9b4ee6fa7e8fafa6455ec594223c
sformichella <sformichella@gmail.com>
2020-10-28 13:51:58 -0700

    add header

--- src/App.js
+++ src/App.js
@@ -1,16 +1,33 @@
 import React, {Component} from 'react';
 import './App.css';
+import Header from './Header.js';
 
-
-import React, { Component } from 'react'
 
 export default class App extends Component {
 
+  state = {
+    searchInput: '',
+    searchParamter: '',
+  }
 
+  handleParameterChange = e => {
+    const parameter = e.target.value;
+    this.setState({
+      searchParamter: parameter
+    })
+  }
+
+  handleInputChange = e => {
+    const text = e.target.value;
+    this.setState({
+      searchInput: text
+    })
+  }
 
   render() {
     return (
       <>
+        <Header />
       </>
     )
   }
